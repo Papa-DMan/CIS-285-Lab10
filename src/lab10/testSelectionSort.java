702e1615e0a6b22bbec0db0556294d0a80f11533
Daniel Mahaney <mahaneyd@umich.edu>
2023-11-30 18:30:54 -0500

    Fix SelectionSort Alg & AdvTests

--- src/lab10/testSelectionSort.java
+++ src/lab10/testSelectionSort.java
@@ -45,9 +45,39 @@ public class testSelectionSort {
 		/** Test data contains negative values only **/
 	}
 	public void testMixed(){
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = -7;
+		arr[3] = -10;
+		arr[4] = 0;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = -10;
+		Sortedarr[1] = -7;
+		Sortedarr[2] = 0;
+		Sortedarr[3] = 8;
+		Sortedarr[4] = 9;
+		SelectionSort s = new SelectionSort();
+		s.basicSelectionSort(arr);
+		assertArrayEquals(Sortedarr, arr);
 		/** Test data contains with both positive, negative and zeros **/
 	}
 	public void testDuplicates(){
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 7;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
+		int[] Sortedarr = new int[5];
+		Sortedarr[0] = 2;
+		Sortedarr[1] = 7;
+		Sortedarr[2] = 7;
+		Sortedarr[3] = 8;
+		Sortedarr[4] = 10;
+		SelectionSort s = new SelectionSort();
+		s.basicSelectionSort(arr);
+		assertArrayEquals(Sortedarr, arr);
 		/** Test data contains duplicates **/
 	}
 }
